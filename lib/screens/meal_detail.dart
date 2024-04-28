4aeddcf03a4b5485969e9311bd561dfd923977b1
aibat-t <tozhybayev.aibat@gmail.com>
2024-04-28 14:58:41 +0500

    added favorite icon change

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -14,6 +14,10 @@ class MealDetailScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoriteMealsProvider);
+
+    final isFavorite = favoriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -33,7 +37,7 @@ class MealDetailScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: const Icon(Icons.star),
+            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
           )
         ],
       ),
